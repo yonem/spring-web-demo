da249a9ae9c206a4d625ea1c24e41e5f8bd62f84
yonem <yonem321@gmail.com>
2023-11-08 07:36:37 +0900

    feat: テーブル作成DDLの初期値の設定 #7

--- vagrant/DDL/create02_todo.sql
+++ vagrant/DDL/create02_todo.sql
@@ -3,9 +3,9 @@
   `category_id` int NOT NULL COMMENT 'カテゴリID',
   `subject` varchar(100) NOT NULL COMMENT '件名',
   `body` text COMMENT '本文',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '登録日付',
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新日付',
-  `deleted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '削除日付',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '登録日付',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新日付',
+  `deleted_at` timestamp DEFAULT NULL COMMENT '削除日付',
   PRIMARY KEY (`id`),
   KEY `categories_todo_fk` (`category_id`),
   CONSTRAINT `categories_todo_fk` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
