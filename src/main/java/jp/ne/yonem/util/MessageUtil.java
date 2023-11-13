4ace7dac8a303c653f8743c1e61d56ad8d8ba9e9
yonem <yonem321@gmail.com>
2023-11-13 18:50:03 +0900

    feat: メッセージ出力共通クラスを実装する #7

---
+++ src/main/java/jp/ne/yonem/util/MessageUtil.java
@@ -0,0 +1,19 @@
+package jp.ne.yonem.util;
+
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+
+public class MessageUtil {
+
+    public static final String E_000 = "E_000";
+    public static final String E_001 = "E_001";
+    public static final String E_101 = "E_101";
+    public static final String E_102 = "E_102";
+    public static final String I_001 = "I_001";
+    public static final String I_101 = "I_101";
+
+    public static String getMessage(MessageSource source, String code, Object... params) {
+        return source.getMessage(code, params, Locale.JAPAN);
+    }
+}
