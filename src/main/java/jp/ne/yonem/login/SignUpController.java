1bfb82b3f04b7d640284f1dc785644ce8c541d91
yonem <yonem321@gmail.com>
2023-11-14 09:12:45 +0900

    feat: ユーザ登録コントローラクラスを実施する #7

---
+++ src/main/java/jp/ne/yonem/login/SignUpController.java
@@ -0,0 +1,78 @@
+package jp.ne.yonem.login;
+
+import jakarta.validation.Valid;
+import jp.ne.yonem.util.MessageUtil;
+import jp.ne.yonem.util.URL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * ユーザ情報登録Controller
+ */
+@Controller
+public class SignUpController {
+
+    private final SignUpService service;
+    private final MessageSource messageSource;
+
+    @Autowired
+    public SignUpController(
+            SignUpService service
+            , MessageSource messageSource
+    ) {
+        this.service = service;
+        this.messageSource = messageSource;
+    }
+
+    @GetMapping(URL.SIGN_UP)
+    String init(Model model) {
+        model.addAttribute("form", new SignUpForm(null, null, null, null));
+        return URL.SIGN_UP;
+    }
+
+    @PostMapping(URL.SIGN_UP)
+    String doSave(Model model, @Valid SignUpForm form, BindingResult bindingResult) {
+        String msg = null;
+        var isError = false;
+
+        try {
+            if (bindingResult.hasErrors()) {
+                var errors = bindingResult.getFieldErrors();
+
+                for (var error : errors) {
+                    model.addAttribute(error.getField() + "Error", error.getDefaultMessage());
+                }
+                isError = true;
+
+            } else {
+
+                if (!form.password.equals(form.reEnter)) {
+                    model.addAttribute("reEnterError", MessageUtil.getMessage(messageSource, MessageUtil.E_102));
+                    isError = true;
+
+                } else {
+                    service.createUser(new UserInfoEntity(form.userName, form.email, form.password));
+                    msg = MessageUtil.getMessage(messageSource, MessageUtil.I_101);
+                }
+            }
+
+        } catch (DataIntegrityViolationException e) {
+            msg = MessageUtil.getMessage(messageSource, MessageUtil.E_101);
+            isError = true;
+
+        } catch (Exception e) {
+            msg = MessageUtil.getMessage(messageSource, MessageUtil.E_000);
+            isError = true;
+        }
+        model.addAttribute("form", form);
+        model.addAttribute("msg", msg);
+        model.addAttribute("isError", isError);
+        return URL.SIGN_UP;
+    }
+}
